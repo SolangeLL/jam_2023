e16ea9317c064c922f5425782a903dffdbe367b8
SolangeLL <solange.le-luel@epitech.eu>
2023-12-03 13:58:24 +0100

    :construction: WIP on adding sounds, TODO type of grounds

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,25 +1,42 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 100.0 
-var can_move = true
+@export var SPEED = 100.0
 @export var coins = 0
+var can_move = true
+var on_ground = false
 const JUMP_VELOCITY = -350.0
+
+var jump_sound : AudioStreamPlayer
+var landing_sound : AudioStreamPlayer
+var walk_sound : AudioStreamPlayer
+
+var detect_type_ground : Area2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$AnimatedSprite2D.play("idle")
+	jump_sound = $SoundEffects/JumpSound
+	landing_sound = $SoundEffects/LandingSound
+	walk_sound = $SoundEffects/WalkSound
+	detect_type_ground = $DetectTypeZone/CollisionShape2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		on_ground = false
+	
+	if is_on_floor() and not on_ground:
+		landing_sound.play()
+		on_ground = true
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		$AnimatedSprite2D.play("jump")
+		jump_sound.play()
 		velocity.y = JUMP_VELOCITY
 
 	handle_movement()
@@ -33,6 +50,15 @@ func _input(event):
 
 	if event.is_action_released("move_left") or event.is_action_released("move_right"):
 		$AnimatedSprite2D.play("stop")
+
+func check_type_ground():
+	if detect_type_ground.get_overlapping_areas().size() > 0:
+		var overlapping_area = detect_type_ground.get_overlapping_areas()[0]
+		var ground_type = overlapping_area.get_groups()
+		
+		if "grass" in ground_type:
+			$SoundEffects/WalkSound.stream = "res://assets/sounds/footsteps_grass.ogg"
+	pass
 
 func handle_movement():
 	if not can_move:
@@ -59,17 +85,12 @@ func respawn():
 	$AnimatedSprite2D.play("idle")
 	var respawn = get_parent().find_child("Respawn")
 	global_position = respawn.global_position
-<<<<<<< HEAD
 	can_move = true
 
 func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation == "stop" or $AnimatedSprite2D.animation == "jump":
 		$AnimatedSprite2D.play("idle")
-	
+
 	if $AnimatedSprite2D.animation == "death":
 		respawn()
 	pass # Replace with function body.
-=======
-	pass
-	
->>>>>>> 70e5c9b0de90d2fd4dc438ec0dda6967042d89a3
