12618b38932c1e939aa37deb400cfbd2927e7e1f
flogrv <florian.grave@epitech.eu>
2023-12-03 18:59:13 +0100

    ADD video_settings fullscreen/windowed

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -59,4 +59,22 @@ func _on_button_video_pressed():
 func _on_button_audio_pressed():
 	hide_all_menus()
 	audio_menu.visible = true
+	
+#les video settings
+func _on_full_screen_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		
+func _on_border_less_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
+func _on_v_sync_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
