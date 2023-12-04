130766856486566ac02a390fcde4bd244d7b88b1
SolangeLL <solange.le-luel@epitech.eu>
2023-12-04 14:19:46 +0100

    :sparkles: Add timer

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+var current_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,13 +8,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Coins.global_position = get_parent().get_screen_center_position()
+	$Coins.global_position =   get_parent().get_screen_center_position()
 	$Coins.global_position.y -= 120
 	$Coins.global_position.x += 170
 	
 	$Deaths.global_position = get_parent().get_screen_center_position()
 	$Deaths.global_position.y += 75
 	$Deaths.global_position.x += 132
+	
+	$Timer.global_position = get_parent().get_screen_center_position()
+	$Timer.global_position.y -= 102
+	$Timer.global_position.x -= 187
 
 func update_coins_text(value : int):
 	$Coins/CoinText.text = str(value)
@@ -24,3 +29,9 @@ func update_death_text(value : int):
 		$Deaths/DeathImage.modulate = Color(255, 0, 0)
 	elif value > 100:
 		$Deaths/DeathImage.modulate = Color(255,255,0)
+
+
+func _on_second_timer_timeout():
+	current_time += 1
+	$Timer/Label.text = "%02d:%02d" % [current_time / 60, current_time  % 60]
+	pass
--- scripts/tacos.gd
+++ scripts/tacos.gd
@@ -5,4 +5,4 @@ func _ready():
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://scenes/win_menu.tscn")
+		get_tree().change_scene_to_file("res://scenes/game_scenes/win_menu.tscn")
